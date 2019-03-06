048a765c4ed87871b4cffbb2e53e1a142e67d61b
Mauricio Garduño <mau.gar.gonz@icloud.com>
2019-03-06 04:20:09 -0600

    hola yisus

---
+++ src/p6/automataalfanumericoarchivo/P6AutomataAlfanumericoArchivo.java
@@ -0,0 +1,20 @@
+package p6.automataalfanumericoarchivo;
+
+import GUI.Main;
+
+/**
+ *
+ * @author Jedidian
+ */
+public class P6AutomataAlfanumericoArchivo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Main inicio = new Main();
+        inicio.setVisible(true);
+    }
+    
+}
